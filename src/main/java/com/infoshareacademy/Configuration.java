09a4bc45cf67e695fcff70edda30b63f7a982e59
Piotr Rudziński <pioru@wp.pl>
2021-04-01 10:06:43 +0200

    kulturalni.properties created

--- src/main/java/com/infoshareacademy/Configuration.java
+++ src/main/java/com/infoshareacademy/Configuration.java
@@ -1,4 +1,23 @@
 package com.infoshareacademy;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Configuration {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
