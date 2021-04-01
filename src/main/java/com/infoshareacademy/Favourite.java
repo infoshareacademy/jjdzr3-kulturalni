f567344844ad902267d425248cb91c8768c32654
Piotr Rudziński <pioru@wp.pl>
2021-04-02 00:40:47 +0200

    Favourities deleted

--- src/main/java/com/infoshareacademy/Favourite.java
+++ src/main/java/com/infoshareacademy/Favourite.java
@@ -1,19 +1,2 @@
 package com.infoshareacademy;
 
-import java.util.List;
-
-public class Favourite {
-    private Integer[] favourities;
-
-    public Favourite(Integer[] favourities) {
-        this.favourities = favourities;
-    }
-
-    public void setFafourities(Integer[] favourities) {
-        this.favourities = favourities;
-    }
-
-    public Integer[] getFavourite() {
-        return favourities;
-    }
-}
