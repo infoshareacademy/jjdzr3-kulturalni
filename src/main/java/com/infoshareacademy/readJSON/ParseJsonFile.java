03b467d5ab7b60d34539c3abbb08a34d6adc0cc4
MaciexDrop <macwla1994@gmail.com>
2021-04-06 21:51:34 +0200

    Yet another code refactor;

--- src/main/java/com/infoshareacademy/readJSON/ParseJsonFile.java
+++ src/main/java/com/infoshareacademy/readJSON/ParseJsonFile.java
@@ -1,6 +1,8 @@
 package com.infoshareacademy.readJSON;
+
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.*;
@@ -12,7 +14,6 @@ public class ParseJsonFile {
     ///ŚCIEŻKA JEST INNA U KAŻDEGO - JESZCZE NIE ROZKMINIŁEM UNIWERSALNEGO ROZWIĄZANIA
 
 
-
     public void displayAllEvents() {
 
         Gson gson = new Gson();
@@ -20,18 +21,24 @@ public class ParseJsonFile {
         try {
             FileReader file = new FileReader("C:\\Users\\macwl\\IdeaProjects\\ISE\\src\\main\\resources\\data.json");
             JsonReader reader = new JsonReader(file);
+
+            //odczytanie stringa z pliku json i umieszczenie każdego z obiektów w macierzy
             jsonObjectTemplate[] data1 = gson.fromJson(reader, jsonObjectTemplate[].class);
+
+            //przekształcenie macierzy w Kolekcję List
             List<jsonObjectTemplate> data2 = Arrays.asList(data1);
 
-            jsonObjectTemplate jsonObjectTemplate = new jsonObjectTemplate();
-
-            for (int i=0; i<data2.size();i++){
-                int eventNr = i+1;
+            for (int i = 0; i < data2.size(); i++) {
+                int eventNr = i + 1;
                 System.out.println();
                 System.out.println("Wydarzenie nr " + eventNr);
                 System.out.println("Nazwa wydarzenia: " + data2.get(i).getName());
                 System.out.println("Organizator: " + data2.get(i).getOrganizer().getDesignation());
-                if (data2.get(i).getActive()==1) {
+
+                //niektóre z wydarzeń posiadają w nazwie "ZAWIESZONE"
+                //Można przemyslec czy powinno miec to wplyw na parametr ACTIVE wydarzenia
+
+                if (data2.get(i).getActive() == 1) {
                     System.out.println("Wydarzenie jest aktualne.");
                 } else
                     System.out.println("Wydarzenie jest nieaktualne.");
@@ -41,7 +48,6 @@ public class ParseJsonFile {
                 System.out.println("Krótki opis: " + data2.get(i).getDescShort());
 
             }
-
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
