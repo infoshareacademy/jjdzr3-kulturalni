9de1871cd3a9f86eeadf7cdb71104292482b1edb
Piotr Rudziński <pioru@wp.pl>
2021-04-09 15:28:36 +0200

    public void readEvent() created

--- src/main/java/com/infoshareacademy/Kulturalni.java
+++ src/main/java/com/infoshareacademy/Kulturalni.java
@@ -1,13 +1,19 @@
 package com.infoshareacademy;
 
-/**
- * Hello world!
- *
- */
-public class Kulturalni
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Kulturalni" );
+
+public class Kulturalni {
+    EventsDB eventsDB;
+    Configuration configuration;
+    Favourities favourities;
+
+    public static void main( String[] args ) {
+        new Kulturalni().doJob(args);
     }
+
+
+    
+
+
+
+
 }
