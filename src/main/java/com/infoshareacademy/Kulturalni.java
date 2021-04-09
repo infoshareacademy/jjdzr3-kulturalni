1d042417d1312a6bc75b8400a2cae5539f9b72f1
Piotr Rudziński <pioru@wp.pl>
2021-04-09 15:35:00 +0200

    public void doJob(String[] args) now exists

--- src/main/java/com/infoshareacademy/Kulturalni.java
+++ src/main/java/com/infoshareacademy/Kulturalni.java
@@ -10,8 +10,20 @@ public class Kulturalni {
         new Kulturalni().doJob(args);
     }
 
+    public void doJob(String[] args) {
+        configuration = new Configuration();
+        configuration.readConfiguration();
+        favourities = new Favourities();
 
-    
+        eventsDB = new EventsDB();
+        eventsDB.readEvent();
+
+        favourities.readFavourities();
+
+        eventsDB.displayEvents("1");    // Tu jest tymczasowo do wywołania wyświetlenia wszystkich eventów, fla których display=1
+
+    }
+
 
 
 
